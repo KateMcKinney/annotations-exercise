5755e07d9efda4ae1ab6150d2fef165f8c57b64b
Kate McKinney <kate.mckinney210@gmail.com>
2020-04-02 11:28:20 -0500

    finished part 2 of exercise

--- src/Author.java
+++ src/Author.java
@@ -1,23 +1,15 @@
-/**
+/*
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
-    }
-
-    /**
-     * @deprecated Use publishedBooks instead
-     */
-    @Deprecated
-    public List<String> getBooks() {
-        return books;
+        books = new ArrayList<>();
     }
 
     public List<String> publishedBooks() {
